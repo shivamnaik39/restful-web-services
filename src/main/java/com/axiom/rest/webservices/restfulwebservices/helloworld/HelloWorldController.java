1a9f3ee1060dd669dc5d743671c9ab85052168c7
Shivam Naik <40293549+shivamnaik39@users.noreply.github.com>
2023-03-22 15:15:37 +0530

    ADD: Swagger Docs and internationalization (i18n)

--- src/main/java/com/axiom/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/axiom/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.axiom.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello")
     public String helloWorld() {
@@ -18,12 +28,16 @@ public class HelloWorldController {
     }
 
     @GetMapping(path = "/hello-bean/{name}")
-    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World : %s", name));
     }
 
-   
+    @GetMapping(path = "/helloworld-int")
+    public String helloWorldInternationalized() {
 
-    
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+    }
 
 }
